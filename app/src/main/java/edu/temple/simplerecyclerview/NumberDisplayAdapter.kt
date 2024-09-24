cb21b7b63453ed1e79497d45c363398d3fb54df2
svtslvtt <tup60276@temple.edu>
2024-09-24 12:02:12 -0400

    onCreateViewHolder implemented, plus getItemCount implemented

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -8,18 +8,20 @@ import androidx.recyclerview.widget.RecyclerView
 
 class NumberDisplayAdapter(private val numbers: IntArray) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>()/* TODO Step 3a: Provide Adapter Parent */ {
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+    class NumberViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = NumberViewHolder (
+        TextView(parent.context).apply {
+            setPadding(5, 5, 5, 5)
+
     }
-
+    )
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return numbers.size
     }
 
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
-        return numbers.size
+        TODO()
     }
 
     // TODO Step 3b: Complete function definitions for adapter
