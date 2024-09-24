6910440f332173385298e9a7f9b4e72f6f903dc2
svtslvtt <tup60276@temple.edu>
2024-09-24 11:55:41 -0400

    Provided Adapter Parent

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,13 +1,26 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter/* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter(private val numnbers: IntArray) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>()/* TODO Step 3a: Provide Adapter Parent */ {
 
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
     // TODO Step 3b: Complete function definitions for adapter
 
